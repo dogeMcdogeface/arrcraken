4aa774ede2bbf6b8442444ce8e893df2b9ab85ab
dogeMcdogeface <79205861+dogeMcdogeface@users.noreply.github.com>
2026-03-08 03:39:19 +0100

    updated export options

--- _builds/html/index.service.worker.js
+++ _builds/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1772937264|2497022847';
+const CACHE_VERSION = '1772937546|2778889242';
 /** @type {string} */
 const CACHE_PREFIX = 'arrcraken-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
